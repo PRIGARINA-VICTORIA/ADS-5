f00ac8f208f96a8b175f4188a1c1fa40fe66e4f8
PRIGARINA-VICTORIA <99180874+PRIGARINA-VICTORIA@users.noreply.github.com>
2022-04-04 00:39:36 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -8,23 +8,23 @@ private:
 T arr[100];
 int top;
 public:
-TStack():top(-1) { }
+TStack(): top(-1) { }
 T get() const {
 return arr[top];
 }
 bool isEmpty() const {
-return top==-1;
+return top == -1;
 }
 bool isFull() const {
-return top==size-1;
+return top == size-1;
 }
 void pop() {
-if(top>=0)
+if(top >= 0)
 top--;
 }
 void push(T item) {
-if(top<size-1)
-arr[++top]=item;
+if(top < size - 1)
+arr[++top] = item;
 }
 };
 #endif  // INCLUDE_TSTACK_H_
